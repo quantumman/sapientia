ca9611c66962eb4ef3d169ba2b3862560aa1f244
Shodai Yokoyama <quantumcars@gmail.com>
2015-12-31 23:55:53 +0900

    Markup container component

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,14 +3,16 @@ import 'styles/App.css';
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
-
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      <div className='index'>
+        <div className='left-container'>
+        </div>
+        <div className='middle-container'>
+        </div>
+        <div className='right-container'>
+        </div>
       </div>
     );
   }
